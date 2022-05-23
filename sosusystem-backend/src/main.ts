5b13f12b2fa135e5cf051c3353597a7dcc092a3d
Anders Madsen <madsenanders97@gmail.com>
2022-05-23 09:18:33 +0200

    Update main.ts

--- sosusystem-backend/src/main.ts
+++ sosusystem-backend/src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.use(cookieParser());
 
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(5500, '0.0.0.0');
 }
 bootstrap();
