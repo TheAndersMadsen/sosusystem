f46f133a7bc327e6aa18db8c45f3528a2d3407de
Anders Madsen <madsenanders97@gmail.com>
2022-05-24 03:20:20 +0200

    Update mongo-config.service.ts

--- sosusystem-backend/src/infrastructure/mongodb/mongo-config.service.ts
+++ sosusystem-backend/src/infrastructure/mongodb/mongo-config.service.ts
@@ -8,12 +8,9 @@ require('dotenv').config();
 @Injectable()
 export class MongooseConfigService implements MongooseOptionsFactory {
   createMongooseOptions(): MongooseModuleOptions {
-    const userName: string = process.env.MONGODB_USER_NAME;
-    const password: string = process.env.MONGODB_USER_PASSWORD;
-    const dbName: string = process.env.MONGODB_DB_NAME;
-    console.log(`mongodb+srv://${userName}:${password}@sosu-db.qc7k5.mongodb.net/${dbName}?retryWrites=true&w=majority`)
+    const dbPort: string = process.env.MONGODB_PORT;
     return {
-      uri: `mongodb://mongo:27017/db`,
+      uri: `mongodb://mongo:${dbPort}/db`,
     };
   }
 }
