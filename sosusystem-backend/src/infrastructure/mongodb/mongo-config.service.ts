39048b0fc3c02ef01a9f57428ed1276848efd9ac
Anders Madsen <madsenanders97@gmail.com>
2022-05-24 23:34:03 +0200

    Update mongo-config.service.ts

--- sosusystem-backend/src/infrastructure/mongodb/mongo-config.service.ts
+++ sosusystem-backend/src/infrastructure/mongodb/mongo-config.service.ts
@@ -9,8 +9,9 @@ require('dotenv').config();
 export class MongooseConfigService implements MongooseOptionsFactory {
   createMongooseOptions(): MongooseModuleOptions {
     const dbPort: string = process.env.MONGODB_PORT;
+    const dbName: string = process.env.MONGODB_NAME;
     return {
-      uri: `mongodb://mongo:${dbPort}/db`,
+      uri: `mongodb://mongo:${dbPort}/${dbName}`,
     };
   }
 }
