3e9056987672bc519235859e209a1bc5529b85f1
Anders Madsen <madsenanders97@gmail.com>
2022-05-27 06:08:30 +0200

    add ui test

--- tests/ui/login-test.js
+++ tests/ui/login-test.js
@@ -1,35 +1,29 @@
-import { ClientFunction, Selector } from 'testcafe';
+import {
+    ClientFunction,
+    Selector
+} from 'testcafe';
 
-fixture `Visit Admin Register Endpoint`
-    .page `http://185.51.76.10:9888/admin/register`;
+fixture `Visit Admin Login Endpoint`
+    .page `http://185.51.76.10:61002/#/login`;
 
-test('Login - Press Create button without providing any information', async t => {
+test("Login - An error is displayed if the e-mail does not exist.", async t => {
     await t
+        .typeText("#loginEmail", "test@test.com")
+        .typeText("#loginPassword", "test123")
+        .takeScreenshot()
         .click("#app > div > form > button")
-        .expect(Selector("#app > div > form > div:nth-child(1) > div").innerText).eql("The Email field is required.")
-        .expect(Selector("#app > div > form > div:nth-child(2) > div").innerText).eql("The Name field is required.")
-        .expect(Selector("#app > div > form > div:nth-child(3) > div").innerText).eql("The Password field is required.")
-        .expect(Selector("#app > div > form > div:nth-child(4) > div").innerText).eql("The PasswordConfirm field is required.")
+        .expect(Selector("#app > div > div").innerText).eql("Login failed, please try again.")
+        .takeScreenshot()
 });
 
 
-test("Login - An error message is displayed if the two passwords don't match.", async t => {
-    await t
-        .typeText("#registerPassword", "test")
-        .typeText("#registerConfirmPassword", "test123")
-        .click("#app > div > form > button")
-        .expect(Selector("#app > div > form > div:nth-child(4) > div").innerText).eql("Passwords do not match")
-});
-
-
-test("Login - When the form is filled successfully you are taken to the login-page.", async t => {
+test("Login - You are taken to the dashboard if you provide correct credentials.", async t => {
     const getLocation = ClientFunction(() => document.location.href);
 
     await t
-        .typeText("#registerEmail", "test@test.com")
-        .typeText("#registerName", "Anders")
-        .typeText("#registerPassword", "testpassword")
-        .typeText("#registerConfirmPassword", "testpassword")
+        .typeText("#loginEmail", "test@gmail.com")
+        .typeText("#loginPassword", "testpassword")
         .click("#app > div > form > button")
-        .expect(getLocation()).contains('http://185.51.76.10:9888/admin/login/?returnUrl=http%3A%2F%2F185.51.76.10%3A9888%2Fadmin');
+    	.expect(getLocation()).contains('http://185.51.76.10:9888/admin/')
+        .takeScreenshot()
 });
