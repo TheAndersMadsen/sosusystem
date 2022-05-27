53921b0f43bfcebfd6700399823fc56b04256b4f
Anders Madsen <madsenanders97@gmail.com>
2022-05-27 06:44:33 +0200

    Update login-test.js

--- tests/ui/login-test.js
+++ tests/ui/login-test.js
@@ -8,22 +8,10 @@ fixture `Visit Admin Login Endpoint`
 
 test("Login - You won't be redirected if account doesn't exist.", async t => {
     await t
-        .typeText("#form2Example17", "anders")
-        .typeText("#form2Example27", "meme")
+        .typeText("#username", "anders")
+        .typeText("#password", "meme")
         .takeScreenshot()
-        .click("#app > div > form > button")
-        .expect(Selector("#app > div > div").innerText).eql("Login failed, please try again.")
-        .takeScreenshot()
-});
-
-
-test("Login - You are taken to the dashboard if you provide correct credentials.", async t => {
-    const getLocation = ClientFunction(() => document.location.href);
-
-    await t
-        .typeText("#loginEmail", "test@gmail.com")
-        .typeText("#loginPassword", "testpassword")
-        .click("#app > div > form > button")
-    	.expect(getLocation()).contains('http://185.51.76.10:9888/admin/')
+        .click("#loginButton")
+    	.expect(getLocation()).contains('http://185.51.76.10:61002/#/dashboard')
         .takeScreenshot()
 });
